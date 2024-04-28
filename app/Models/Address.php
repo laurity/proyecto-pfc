646b25e166bb8d219c6430c4912c01e678268d11
Laura López <99351746+laurity@users.noreply.github.com>
2024-04-28 21:06:01 +0200

    instalado panel admin

--- app/Models/Address.php
+++ app/Models/Address.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Address extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['order_id', 'first_name', 'last_name', 'phone', 'street_address', 'city', 'province', 'postal_code'];
+
+    public function order()
+    {
+        return $this->belongsTo(Order::class);
+    }
+
+    public function getFullNameAttribute() //Esta funcion hace que se pueda acceder a la propiedad full_name en vez de first_name y last_name
+    {
+        return "{$this->first_name} {$this->last_name}";
+    }
 }
